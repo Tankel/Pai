bbda001b3697dc518fa430e373baa63eaa139a32
Tankel <luisarf2222@gmail.com>
2022-10-20 15:15:08 -0500

    new changed

--- Main.cpp
+++ Main.cpp
@@ -14,7 +14,8 @@ int main()
     {
       cout<<"Quieres nombrar a tu nuevo amigo? s/n\n";
       std::cin>>r;
-      p = Pai();
+      //p = Pai();
+      p = Pai("Pai", 100, 3);
       if(r=='S' || r=='s')
       {
         cout<<"Ingrese el mote\n";
